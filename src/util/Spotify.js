c6ad98a92ff179cb921a5ea91f50e0564d8661ab
George <georgediepiriye4u@gmail.com>
2021-11-19 14:21:17 +0100

    added a surge redirect uri

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -2,8 +2,7 @@
 
 
 let accessToken = '';
-const CLIENT_ID = 'e20f72a0f82149eda7e81420e7cd9bc5';
-const REDIRECT_URI = "http://localhost:3000/";
+const REDIRECT_URI = "http://jammingg.surge.sh";
 const Spotify={
     savePlaylist(name,tracksUris){
         if(!name || !tracksUris.length){
@@ -77,7 +76,7 @@ const Spotify={
            
 
         } else{
-            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`
             window.location = accessUrl
         }
  
